dbe6a999bb9a28826a3535831045994ee8a2a546
Dmitrii Deviatilov <ddevyatilov@mfms.ru>
2022-06-25 13:34:21 +0700

    Removed facebook&twitter

--- app/src/main/kotlin/com/d9tilov/moneymanager/splash/ui/SplashFragment.kt
+++ app/src/main/kotlin/com/d9tilov/moneymanager/splash/ui/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.d9tilov.moneymanager.splash.ui
 
 import android.content.Intent
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
@@ -24,7 +25,7 @@ class SplashFragment :
 
     private val providers = arrayListOf(
         AuthUI.IdpConfig.PhoneBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build()
+        AuthUI.IdpConfig.GoogleBuilder().build(),
     )
 
     override fun getNavigator() = this
