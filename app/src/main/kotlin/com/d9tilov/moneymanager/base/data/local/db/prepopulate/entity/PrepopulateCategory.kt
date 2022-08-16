f4412c7157120f52be4447a594118cd1d1ba94b6
Dmitrii Deviatilov <ddevyatilov@mfms.ru>
2022-08-16 21:15:09 +0700

    Fixed issue with remote config

--- app/src/main/kotlin/com/d9tilov/moneymanager/base/data/local/db/prepopulate/entity/PrepopulateCategory.kt
+++ app/src/main/kotlin/com/d9tilov/moneymanager/base/data/local/db/prepopulate/entity/PrepopulateCategory.kt
@@ -2,7 +2,6 @@ package com.d9tilov.moneymanager.base.data.local.db.prepopulate.entity
 
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import com.d9tilov.moneymanager.transaction.domain.entity.TransactionType
 
 data class PrepopulateCategory(
