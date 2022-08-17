3daee08e8da578fcd836276ecd2004aae682dddd
Dmitrii Deviatilov <ddevyatilov@mfms.ru>
2022-08-17 08:50:20 +0700

    Increased targetSdk && fixed remote config fetching

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,13 +1,13 @@
 object ConfigData {
-    const val compileSdkVersion = 32
-    const val buildToolsVersion = "32.0.0"
+    const val compileSdkVersion = 33
+    const val buildToolsVersion = "33.0.0"
     const val minSdkVersion = 21
-    const val targetSdkVersion = 32
+    const val targetSdkVersion = 33
 
     private const val versionMajor = 1
     private const val versionMinor = 0
     private const val versionPatch = 2
-    private const val versionBuild = 14
+    private const val versionBuild = 15
     const val versionCode = 1000 * (1000 * versionMajor + 100 * versionMinor + versionPatch) + versionBuild
     const val versionName = "${versionMajor}.${versionMinor}.${versionPatch}.${versionBuild}"
 }
