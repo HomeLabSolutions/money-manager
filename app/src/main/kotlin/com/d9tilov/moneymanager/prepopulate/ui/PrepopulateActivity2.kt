60f0880bf9d248c5f534fa28ea85e384689a4ab6
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2022-10-18 20:57:02 +0700

    Added screens to prepopulate

--- app/src/main/kotlin/com/d9tilov/moneymanager/prepopulate/ui/PrepopulateActivity2.kt
+++ app/src/main/kotlin/com/d9tilov/moneymanager/prepopulate/ui/PrepopulateActivity2.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,12 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material3.ButtonDefaults
@@ -38,15 +33,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.d9tilov.moneymanager.R
 import com.d9tilov.moneymanager.budget.domain.entity.BudgetData
 import com.d9tilov.moneymanager.currency.domain.entity.DomainCurrency
 import com.d9tilov.moneymanager.theme.MoneyManagerTheme
@@ -68,9 +57,19 @@ class PrepopulateActivity2 : ComponentActivity() {
 
 @Composable
 fun PrepopulateScreen(uiState: PrepopulateUiState) {
-    var screenType by remember { mutableStateOf(PrepopulateScreen.CURRENCY) }
     Column {
-        CurrencyListContainer(uiState.currencyList, modifier = Modifier.weight(1f))
+        var screenType: PrepopulateScreen by remember { mutableStateOf(PrepopulateScreen.CURRENCY) }
+        when (screenType) {
+            PrepopulateScreen.CURRENCY -> CurrencyListContainer(
+                uiState.currencyList,
+                modifier = Modifier.weight(1f)
+            )
+            PrepopulateScreen.BUDGET -> BudgetContainer(
+                uiState.budgetData,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -82,11 +81,21 @@ fun PrepopulateScreen(uiState: PrepopulateUiState) {
                 modifier = Modifier.fillMaxWidth(0.5f),
                 text = "Progress"
             )
-            IconButton(onClick = {  },
+            IconButton(
+                onClick = {
+                    screenType = when (screenType) {
+                        PrepopulateScreen.CURRENCY -> PrepopulateScreen.BUDGET
+                        else -> PrepopulateScreen.CURRENCY
+                    }
+                },
                 modifier = Modifier
                     .size(40.dp)
                     .padding(horizontal = 4.dp)
-                    .border(1.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(10)),
+                    .border(
+                        1.dp,
+                        MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(10)
+                    ),
             ) {
                 Icon(
                     Icons.Default.ArrowDropDown,
@@ -96,7 +105,9 @@ fun PrepopulateScreen(uiState: PrepopulateUiState) {
             }
             OutlinedButton(
                 onClick = { },
-                modifier = Modifier.height(height = 40.dp).wrapContentWidth(),
+                modifier = Modifier
+                    .height(height = 40.dp)
+                    .wrapContentWidth(),
                 border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
                 shape = RoundedCornerShape(10),
                 colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colorScheme.primary)
