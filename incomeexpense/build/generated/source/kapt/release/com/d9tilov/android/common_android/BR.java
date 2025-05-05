f8176a5d9ca9a3211cae08eb7169b945fc986bc3
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2025-05-05 21:36:56 +0700

    Removed build folders

--- incomeexpense/build/generated/source/kapt/release/com/d9tilov/android/common_android/BR.java
+++
@@ -1,5 +0,0 @@
-package com.d9tilov.android.common_android;
-
-public class BR {
-  public static int _all = 0;
-}
--- incomeexpense/build/tmp/kapt3/stubs/release/com/d9tilov/android/incomeexpense/navigation/ExpenseNavigator.java
+++
@@ -1,7 +0,0 @@
-package com.d9tilov.android.incomeexpense.navigation;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lcom/d9tilov/android/incomeexpense/navigation/ExpenseNavigator;", "Lcom/d9tilov/android/incomeexpense/navigation/BaseIncomeExpenseNavigator;", "incomeexpense_release"})
-public abstract interface ExpenseNavigator extends com.d9tilov.android.incomeexpense.navigation.BaseIncomeExpenseNavigator {
-}
