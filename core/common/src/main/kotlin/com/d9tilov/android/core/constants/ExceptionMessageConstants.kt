419e12b2e4fdf40142049f19b2a5b08a7338987d
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2025-05-18 20:11:02 +0700

    Added analytics to Backup flow

---
+++ core/common/src/main/kotlin/com/d9tilov/android/core/constants/ExceptionMessageConstants.kt
@@ -0,0 +1,7 @@
+package com.d9tilov.android.core.constants
+
+object ExceptionMessageConstants {
+    const val UID_IS_NULL_OR_EMPTY = "Uid is null or empty"
+    const val NETWORK_EXCEPTION = "Network exception"
+    const val FILE_NOT_FOUND = "File not found"
+}
