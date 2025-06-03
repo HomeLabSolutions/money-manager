f605da53a5f5738b19b015ccf08e6bb514d29bb3
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2025-06-03 17:52:55 +0700

    Added time to transaction item in statistics

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ extra["targetSdkVersion"] = 35
 extra["versionMajor"] = 1
 extra["versionMinor"] = 1
 extra["versionPatch"] = 1
-extra["versionBuild"] = 6
+extra["versionBuild"] = 7
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
