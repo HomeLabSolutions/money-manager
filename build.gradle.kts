b8d18a78062f0707bec81c6332200ad3c7bc8b08
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2025-06-21 22:23:57 +0700

    Updated app version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ extra["targetSdkVersion"] = 35
 extra["versionMajor"] = 1
 extra["versionMinor"] = 1
 extra["versionPatch"] = 1
-extra["versionBuild"] = 10
+extra["versionBuild"] = 11
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
