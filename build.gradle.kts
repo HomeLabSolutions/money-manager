91fc581b4077253ae0ae07de5d5de922bd7e42cd
Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>
2025-06-29 20:37:50 +0700

    Try to fix Worker

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ extra["targetSdkVersion"] = 35
 extra["versionMajor"] = 1
 extra["versionMinor"] = 1
 extra["versionPatch"] = 1
-extra["versionBuild"] = 12
+extra["versionBuild"] = 13
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
