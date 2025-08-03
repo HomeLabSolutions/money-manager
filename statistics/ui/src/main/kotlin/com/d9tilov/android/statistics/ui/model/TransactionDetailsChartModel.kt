6d888cf06fc07bddbbb6b0f3cb6c1a83b0068055
Dmitrii Deviatilov <5618882+9tilov@users.noreply.github.com>
2025-08-03 22:36:41 +0700

    Added arrows to statistics (#34)
    Co-authored-by: Dmitrii Deviatilov <dmitrii.deviatilov@agoda.com>

--- statistics/ui/src/main/kotlin/com/d9tilov/android/statistics/ui/model/TransactionDetailsChartModel.kt
+++ statistics/ui/src/main/kotlin/com/d9tilov/android/statistics/ui/model/TransactionDetailsChartModel.kt
@@ -2,7 +2,5 @@ package com.d9tilov.android.statistics.ui.model
 
 data class TransactionDetailsChartModel(
     val categoryId: Long,
-    val from: Long,
-    val to: Long,
     val inStatistics: Boolean,
 )
