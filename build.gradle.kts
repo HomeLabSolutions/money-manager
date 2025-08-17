86f8c896369023b15fda43f860f283d1b8c9bb89
ddeviatilov <9.tilov@gmail.com>
2025-08-17 16:44:00 +0700

    New release 1.1.1.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ extra["targetSdkVersion"] = 36
 extra["versionMajor"] = 1
 extra["versionMinor"] = 1
 extra["versionPatch"] = 1
-extra["versionBuild"] = 24
+extra["versionBuild"] = 25
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
