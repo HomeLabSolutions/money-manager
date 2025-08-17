fb52f7fc3225f5d24c59e206f30bd04ce083fd94
Dmitrii Deviatilov <5618882+9tilov@users.noreply.github.com>
2025-08-17 16:43:22 +0700

    Added some analytics events (#96)

--- analytics/domain/src/main/kotlin/com/d9tilov/android/analytics/model/AnalyticsParams.kt
+++ analytics/domain/src/main/kotlin/com/d9tilov/android/analytics/model/AnalyticsParams.kt
@@ -3,19 +3,9 @@ package com.d9tilov.android.analytics.model
 sealed class AnalyticsParams(
     val name: String,
 ) {
-    sealed class MainScreen(
-        name: String,
-    ) : AnalyticsParams("main_$name") {
-        data object EditMode : AnalyticsParams("edit_mode")
+    data object MainScreenEditMode : AnalyticsParams("main_screen_edit_mode")
 
-        data object ScreenType : AnalyticsParams("screen_type")
-    }
-
-    sealed class CategoryScreen(
-        name: String,
-    ) : AnalyticsParams("category_$name") {
-        data object ScreenType : AnalyticsParams("screen_type")
-    }
+    data object MainScreenType : AnalyticsParams("main_screen_type")
 
     data object LoginResultProvider : AnalyticsParams("login_provider_type")
 
