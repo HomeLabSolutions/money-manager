f2fda379d54882441b36cf8b41aba18a5f9e9885
ddeviatilov <9.tilov@gmail.com>
2025-08-21 22:28:24 +0700

    Fixed crash + show app version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,8 @@ extra["minSdkVersion"] = 23
 extra["targetSdkVersion"] = 36
 extra["versionMajor"] = 1
 extra["versionMinor"] = 1
-extra["versionPatch"] = 1
-extra["versionBuild"] = 26
+extra["versionPatch"] = 2
+extra["versionBuild"] = 3
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
