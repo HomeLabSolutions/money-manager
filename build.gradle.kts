384172288fb3c5bdf6eec7eea9dd98a746a2219d
d9tilov <9.tilov@gmail.com>
2026-09-24 19:37:14 +0100

    Bump app version to 1.2.26.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ extra["minSdkVersion"] = 24
 extra["targetSdkVersion"] = 36
 extra["versionMajor"] = 1
 extra["versionMinor"] = 2
-extra["versionPatch"] = 25
+extra["versionPatch"] = 26
 extra["versionBuild"] = 1
 
 tasks.register("clean", Delete::class) {
